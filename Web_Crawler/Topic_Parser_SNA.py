34e50aa96aae1c94bbb1c55a43f9e504f16662e8
rlrlaa123 <kimdonghyun3366@gmail.com>
2017-09-26 17:06:50 +0900

    TopicParser: Update writing csv

--- Web_Crawler/Topic_Parser_SNA.py
+++ Web_Crawler/Topic_Parser_SNA.py
@@ -9,8 +9,11 @@ with open('Repository_data.csv','rb') as csvfile:
     for row in reader:
         regex = re.compile("'(.+?)'")
         topic = regex.findall(row['Topic'])
-        if not topic :
-            empty += 1
-        else:
-            valid += 1
+        with open ('Topic_data.csv','a') as csvfile:
+            writer = csv.writer(csvfile)
+            if not topic :
+                empty += 1
+            else:
+                valid += 1
+                writer.writerow([row['full_name']]+topic)
     print empty, valid
